c21c394b9ff3363ab1f15de6995ab0358cd7662c
Johannes Filter <hi@jfilter.de>
2022-03-24 13:15:08 +0100

    Fix parser

--- check24-scraper/check24scraper/scrape.py
+++ check24-scraper/check24scraper/scrape.py
@@ -35,41 +35,44 @@ def get_jobs():
 def parse_html(html):
     results = []
 
-    for row in html.find_all(".c24-result-row"):
+    for row in html.select(".c24-result-row"):
+        if not "data-tariff-id" in row.attrs:
+            continue
+
         result = {}
-        result["tariff"] = row.find(".c24-result-row__content__provider__name").text
-        result["provider"] = row.find(
-            ".c24-result-row__content__provider__logo__image"
-        ).get("alt")
-        result["price"] = row.find(".rlv-price__amount").text
+        result["tariff_id"] = row.attrs["data-tariff-id"]
+        result["tariff_name"] = row.attrs["data-tariff-name"]
+        result["tariff_variation"] = row.attrs["data-tariff-variation-key"]
+        result["tariff_grade"] = row.attrs["data-tariff-grade"]
+        result["promotion_position"] = row.attrs["data-promotion-position"]
+        result["provider"] = row.attrs["data-tariff-provider"]
+        result["price"] = row.select_one(".rlv-price__amount").attrs[
+            "data-price-primary"
+        ]
         results.append(result)
 
     return results
 
 
 def _get_tariff(oc, dob, smoker):
-
     lookup = {"oc_id": oc[0], "oc_name": oc[1], "dob": dob, "smoker": smoker}
-    print(lookup)
 
     exists = tab_res.find_one(**lookup)
     if exists:
         return
-    print(exists)
+
     # special for 'never smoked'
     nonsmokeryears = 255
 
     url = f"https://vorsorge.check24.de/risikoleben/vergleichsergebnis/?c24api_rs_lang=&c24api_rs_session=&c24login_type=none&c24api_realestateproprietor=no&c24api_insurepersondiffers=no&c24api_children_discount=no&c24api_loannotolderthansixmonths=no&c24_controller=result&c24_calculate=x&c24api_currentinsurancetype=&c24api_smoker={smoker}&c24api_nonsmokeryears={nonsmokeryears}&c24api_birthdate={dob}&c24api_protectiontype=constant&c24api_protectiontarget=family&c24api_occupation_id={oc[0]}&c24api_sum_course=decreasing_linearly&c24api_sortfield=price&c24api_sortorder=asc&has_filter_active=false&c24api_insure_sum=200.000&c24api_insure_period=20&c24api_paymentperiod=month&c24api_occupation_name={oc[1]}&c24api_insure_date=2022-04-01"
     html = fetch(url)
-    print(url)
-    # print(html)
 
     if html is None:
         raise Exception("fuck")
 
     results = parse_html(html)
     lookup["created_at"] = datetime.utcnow()
-    results = [r | lookup for r in results]
+    results = [r | lookup | {"rank": i} for i, r in enumerate(results)]
 
     print(results)
 
